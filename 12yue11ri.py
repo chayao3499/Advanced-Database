24b70ab62d09678c8813127d2d0ea4b1fc8f1fce
chayao3499 <1299039264@qq.com>
2016-12-23 13:31:14 +0800

    Update 12yue11ri.py

--- 12yue11ri.py
+++ 12yue11ri.py
@@ -6,13 +6,16 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-url = 'http://www.usst.edu.cn/s/1/t/517/p/2/list.htm'
-content = urllib2.urlopen(url).read()
-soup = BeautifulSoup(content, from_encoding = 'utf-8')
-hotNews = soup.find_all('div', {'class', 'column_list_content'})[0].find_all('tr')
-f = open(r'd:/xinwen.txt','a')
-for i in hotNews:
-    f.write('\n' + i.a.text)
-    print i.a.text
-    print i.a['href']
-f.close()
+url_1 = 'http://www.usst.edu.cn/s/1/t/517/p/2/i/382/list.htm'
+page = url_1[-12:-9]
+for i in range(1,int(page)):
+    url = "http://www.usst.edu.cn/s/1/t/517/p/2/i/{0}/list.htm" . format(i)
+    content = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(content, from_encoding = 'utf-8')
+    hotNews = soup.find_all('div', {'class', 'column_list_content'})[0].find_all('tr')
+    f = open(r'd:/xinwen.txt','a')
+    for i in hotNews:
+        f.write('\n' + i.a.text)
+        print i.a.text
+        print i.a['href']
+    f.close()
